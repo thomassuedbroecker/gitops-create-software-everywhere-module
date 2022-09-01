d583d130b337a16e5c969a5eef9776919ca458dc
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-09-01 17:00:04 +0200

    update

--- example/helper-create-scaffolding.sh
+++ example/helper-create-scaffolding.sh
@@ -10,15 +10,15 @@ CUSTOM_CATALOG=https://raw.githubusercontent.com/thomassuedbroecker/gitops-terra
 iascable build -i ibm-vpc-roks-argocd-guestbook.yaml -c $BASE_CATALOG -c $CUSTOM_CATALOG
 
 # 2. Copy helper bash scipts into the output folder
-cp helper-tools-create-container-workspace.sh ./output
-cp helper-tools-execute-apply-and-backup-result.sh ./output
-cp helper-tools-execute-destroy-and-delete-backup.sh ./output
+# cp helper-tools-create-container-workspace.sh ./output
+# cp helper-tools-execute-apply-and-backup-result.sh ./output
+# cp helper-tools-execute-destroy-and-delete-backup.sh ./output
 
 # 3. Navigate to created 'scaffolding'
-cd output
+# cd output
 
 # 4. Start container engine
-"${CONTAINER_ENGINE}" start
+# "${CONTAINER_ENGINE}" start
 
 # 5. Start tools container
-sh launch.sh
+# sh launch.sh
