d79e89709c0fa7315197524ed052ec09d08f4822
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-09-06 09:49:13 +0200

    In the BOM we changed the version for: ibm-vpc-subnets from v1.13.3 to v1.13.2 ibm-object-storage v4.0.6 to v4.0.3 gitops-repo v1.21.0 to version: v1.20.2 That was the reason why the API key request appears again

--- example/helper-tools-create-container-workspace.sh
+++ example/helper-tools-create-container-workspace.sh
@@ -14,6 +14,12 @@ mkdir ${WORKSPACES_PATH}
 mkdir ${WORKSPACES_PATH}/${PROJECT_NAME}
 
 # 2. Copy project into the workspace
-cp -R . ${WORKSPACES_PATH}
+# 2.1 Copy the helper scripts
+cp -R . ${WORKSPACES_PATH} 
+# 2.1 Copy the project content to the new workspace
 cp ./${PROJECT_NAME}/*.* ${WORKSPACES_PATH}/${PROJECT_NAME}
+# 2.3 Navigate to the new project in the new workspace
 cd ${WORKSPACES_PATH}/${PROJECT_NAME}
+
+# (optional) 3. Login to IBM Cloud
+# ibmcloud login -sso
